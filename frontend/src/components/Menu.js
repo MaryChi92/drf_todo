15d25c43bfba4084d2a057754d136feeaaa68ca8
MaryChi92 <pereverzeva.mary@gmail.com>
2023-02-08 20:52:46 +0300

    some more files

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Menu = () => {
+    return (
+        <menu>
+            <li>
+                <a href="">Все пользователи</a>
+            </li>
+            <li>
+                <a href="">Проекты</a>
+            </li>
+            <li>
+                <a href="">To-Do</a>
+            </li>
+        </menu>
+    )
+}
+
+export default Menu
