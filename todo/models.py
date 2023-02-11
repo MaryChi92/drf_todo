db2d9a3b7b9e255e3e0bd8af49d76cdf0e222991
MaryChi92 <pereverzeva.mary@gmail.com>
2023-02-11 18:04:49 +0300

    more files for lesson 3

---
+++ todo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from users.models import User
+
+
+class Project(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=64)
+    repository = models.URLField(verbose_name='Ссылка на репозиторий', blank=True)
+    users = models.ManyToManyField(User, verbose_name='Пользователи')
+
+
+class ToDo(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    text = models.TextField(verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создана')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлена')
+    created_by_user = models.ForeignKey(User, verbose_name='Создана (юзер)', on_delete=models.PROTECT)
+    is_active = models.BooleanField(default=True, verbose_name='Активна')
